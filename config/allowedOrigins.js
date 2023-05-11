fefd90292d1c4d31894ea0005e729e3d78805f1e
William Rainey <williamhrainey@gmail.com>
2023-05-11 15:08:19 -0700

    added middleware to REST api

---
+++ config/allowedOrigins.js
@@ -0,0 +1,9 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.aretahitioutriggerworld.com',
+    'https://aretahitioutriggerworld.com',
+    'https://www.areoutriggerworld.com',
+    'https://areoutriggerworld.com',
+]
+
+module.exports = allowedOrigins
